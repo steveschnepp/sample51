98126deb7f2d5adc06ffc58c35585e5c3031a55d
Steve Schnepp <steve.schnepp@pwkf.org>
2023-09-19 01:03:50 +0200

    stack.c

---
+++ stack.c
@@ -0,0 +1,20 @@
+// #include <at89x051.h>
+volatile unsigned char P1;
+volatile unsigned char P3;
+
+int fib(int n)
+{
+	if (n <= 1) {
+		return n;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
+void main(void)
+{
+	for (unsigned char i = 0; i < 255; i++) {
+		int f = fib(i);
+		P1 = f >> 8;
+		P3 = f;
+	}
+}
