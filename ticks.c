db44a62c2ea40fa4d5f20187e223651628f2f942
Steve Schnepp <steve.schnepp@pwkf.org>
2023-09-26 22:17:43 +0200

    adding timer to debug multi-cycle instructions

--- ticks.c
+++ ticks.c
@@ -1,13 +1,20 @@
 #include <stdint.h>
 #include <8051.h>
 
-void main() {
-    uint8_t a = 1;
-    uint8_t b = 1;
+int dummy_call(void) {
+	return 1;
+}
+
+void main(void) {
+    // STARTING T0 as we want to see the timers even on multi-cycle instructions
+    TCON=0x10;
+
+    uint8_t a = dummy_call();
+    uint8_t b = dummy_call();
     while (1) {
         uint16_t c = a * b;
-        a += c;
-        b ^= a;
+        a += c & 0xFF;
+        b ^= c >> 8;
     }
 }
 
